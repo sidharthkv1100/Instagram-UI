b2aec7bd3ebaecc3cbe1c13cd876237c26603a5e
Sidharth K V <sidharthkv1100@gmail.com>
2026-01-01 22:30:22 +0530

    Create message_bubble.dart

---
+++ lib/widgets/message_bubble.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String text;
+  final bool isMe;
+
+  const MessageBubble({
+    super.key,
+    required this.text,
+    required this.isMe,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: isMe ? Colors.blue : Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: isMe ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
